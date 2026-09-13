8a55bae481e444e271f551ec7d04ba1290a44731
nickleyw <nickleyw@gmail.com>
2026-09-13 09:18:22 -0400

    Refresh web app cache

--- Mage.WebBridge/src/main/resources/web/service-worker.js
+++ Mage.WebBridge/src/main/resources/web/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'xmage-web-bridge-shell-v9';
+const CACHE = 'xmage-web-bridge-shell-v10';
 const SHELL = ['/', '/index.html', '/styles.css', '/app.js', '/manifest.webmanifest', '/app-icon.svg', '/apple-touch-icon.png'];
 
 self.addEventListener('install', event => {
